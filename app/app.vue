fdd1245a9469d947891b0250139ef366eec49408
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2025-08-20 19:45:00 +0800

    ✨ feat: Adds Vercel analytics and speed insights
    Adds Vercel analytics and speed insights to track application performance and user behavior. This provides valuable data for optimizing the user experience.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
+    <Analytics />
+    <SpeedInsights />
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+<script lang="ts" setup>
+import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
