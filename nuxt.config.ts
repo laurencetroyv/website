0a4fec0df7addb4c8a9825ff5629ab9a35cba614
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2025-08-20 19:59:43 +0800

    ✨ feat: Adds PrimeVue and Tailwind CSS integration
    Configures PrimeVue and Tailwind CSS for the application.
    
    This includes installing necessary dependencies, setting up CSS imports, and configuring the Nuxt configuration file to use PrimeVue components and Tailwind CSS styling.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,53 @@
+import tailwindcss from "@tailwindcss/vite";
+import Aura from '@primeuix/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+
+    css: ['~/assets/css/main.css'],
+
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
+
+    modules: ['@primevue/nuxt-module'],
+
+    primevue: {
+        components: {
+            prefix: 'Primevue'
+        },
+        options: {
+            ripple: true,
+            inputVariant: 'filled',
+            darkModeSelector: 'light',
+            theme: {
+                options: {
+                    cssLayer: {
+                        name: 'primevue',
+                        order: 'theme, base, primevue'
+                    }
+                },
+                preset: {
+                    ...Aura.semantic,
+                    primary: {
+                        50: "#e9f5fc",
+                        100: "#d3ebf8",
+                        200: "#a6d7f2",
+                        300: "#7ac3eb",
+                        400: "#4eb0e4",
+                        500: "#219cde",
+                        600: "#1b7db1",
+                        700: "#145d85",
+                        800: "#0d3e59",
+                        900: "#071f2c",
+                        950: "#031016",
+                    }
+                }
+            },
+        },
+    }
 })
